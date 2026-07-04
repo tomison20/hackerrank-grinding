830d55138ca3cbdd2cf804ec56bf8a3c031464e1
Tom Ison <150614474+tomison20@users.noreply.github.com>
2026-07-04 22:07:25 +0530

    Added solution - LeetHub

---
+++ 22727-write-a-function-Hackerrank/22727-write-a-function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    leap = False
+
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+
+    return leap
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
